df9e1aceb20a6c415aebf611683784c394b3dd12
DiabloAzul <curr33@gmail.com>
2019-07-28 15:11:16 -0600

    initial files and edits to burger

---
+++ burger/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('PB & Jealous');
+INSERT INTO burgers (burger_name) VALUES ('Cowboy Crunch');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tasty Teriyaki', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pacific Pineapple', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Magical Mushroom & Swiss', true);
+INSERT INTO burgers (burger_name) VALUES ('The Classic');
